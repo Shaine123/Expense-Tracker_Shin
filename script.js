f1e3ec42ad3ff10ae11ca8668486adaa59043d69
Shaine123 <shaineberdida@gmail.com>
2026-05-11 17:25:03 +0800

    added the adding value logic

--- script.js
+++ script.js
@@ -7,6 +7,29 @@ const form = document.getElementById("submit-form");
 
 form.addEventListener("submit", addTransactions);
 
+const transactions = [];
+
 function addTransactions(e) {
   e.preventDefault();
+
+  const formData = new FormData(form);
+
+  const description = formData.get("description");
+  const amount = formData.get("amount");
+
+  transactions.push({
+    id: Date.now(),
+    description,
+    amount,
+  });
+
+  updateIncomeSummary();
+}
+
+function updateIncomeSummary() {
+  const balanceVal = transactions.reduce((acc, curr) => {
+    return acc + curr.amount;
+  }, 0);
+
+  balance.textContent = parseInt(balanceVal);
 }
